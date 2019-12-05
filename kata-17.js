d820b992ca5792915c65d88192ed54a7aa1d5a82
Eric-Lombardo <ejlanim8or@gmail.com>
2019-12-05 02:51:10 +0000

    added basis functionality of too high/ too low/you got it

--- kata-17.js
+++ kata-17.js
@@ -1,5 +1,21 @@
 let prompt = require("prompt-sync")();
 
 // code below (replace this example)
-let answer = prompt("Guess a number: ");
-console.log("You answered: " + answer);
+
+// creates random number from 1 to 100
+let answer = Math.floor(Math.random() * 101);
+let isGuessed = false;
+
+while (isGuessed === false) {
+  let guess = prompt("Guess a number: ");
+
+  // using == because answer is num, but guess is string type
+  if (guess == answer) {
+    console.log("You got it!");
+    isGuessed = true;
+  } else if (guess > answer) {
+    console.log("Too High!");
+  } else if (guess < answer) {
+    console.log("Too Low!");
+  }
+}
